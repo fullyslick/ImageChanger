a8fbf2727cff6e16ceb2a3f5c5c7793d6d3d00b7
default <default@mail.com>
2018-02-13 12:57:40 +0200

    Add active class on small images.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ImageChanger
-Switches background of the banner, creates slide show.
+Switches background of the banner, creates slide show, small images below the banner are highlighted depending on the banner.
--- myScripts.js
+++ myScripts.js
@@ -14,30 +14,39 @@ $(document).ready(function() {
     banner.removeClass();
     //Add a class with the new background image
     banner.addClass('banner-' + currentImage);
+
+    // Loop through all service elements and remove the active class
+    for (var i = 0; i < services.length; i++) {
+      $(services[i]).removeClass('active');
+    }
+    //Make only the current service .active
+    $(services[currentImage-1]).addClass('active');
   }
 
-  //Loop function that switches images
+  //Loop function that switches images automaticly
   function slideShow() {
-    // Call a 3s setTimeout when the loop is called
+    // This will cause the delay between switching images
     setTimeout(function() {
-      // Change background
+      // Call changeBackground() to swotch the images
       changeBackground();
+
       currentImage++;
-      //  if the index < the number of the services, call the loop function
+      //  if the currentImage < the number of the services, call the slideShow function again
       if (currentImage < services.length + 1) {
-        slideShow(); //  ..  again which will trigger another
+        slideShow();
       } else {
-        // Return to first image by setting the currentImage to 1
+        // It is now on the last image so reset the currentImage to 1
+        // And start the slideShow again
         currentImage = 1;
         slideShow();
       }
     }, 3000) //  ..  setTimeout() delay between image switching
   }
 
-  // Call change background to set the first default image
+  // Call change background to set the first default image on page load
   changeBackground()
-  // Set the currentImage to next one to prevent double loading of the first image
+  // Set the currentImage to the second to prevent double loading of the first image in the slideShow
   currentImage++;
-  // Execute the slide show
+  // Now execute the slide show
   slideShow();
 });
