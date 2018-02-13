07710835c9b66c9181b65126aeaf204d52750e5a
default <default@mail.com>
2018-02-13 11:37:21 +0200

    Simple background switcher.

--- myScripts.js
+++ myScripts.js
@@ -1,65 +1,43 @@
 $(document).ready(function() {
-  // Get the width of the windiw and assign it to a  variable
-  var width = $(window).width();
-
-  //  Listen for on change of screen size
-  $(window).resize(function() {
-    width = $(window).width();
-    console.log(width);
-  });
-
-  // Get the services links-images
+  // Get the services links-images, to know how clases holding images should be loop through
   const services = $(".service");
   // Get the banner
   const banner = $("#banner");
-  // Holds the current banner image number
-  var currentNumber = 1;
-
-  // If it is in desktop mode change background, but do not go to link
-  // if it is in mobile mode work as normal link
-  services.click(function(e) {
-    console.log("test");
-    if (width > 650) {
-      e.preventDefault();
-      var clickedElement = $(e.target);
-      console.log(clickedElement);
-      if (clickedElement.is('#service-1')) {
-        currentNumber = 1;
-      } else if (clickedElement.is('#service-2')) {
-        currentNumber = 2;
-      } else if (clickedElement.is('#service-3')) {
-        currentNumber = 3;
-      } else if (clickedElement.is('#service-4')) {
-        currentNumber = 4;
-      }
-      // Change Background
-      changeBackground();
-    } else {
-      return true;
-    }
-  });
+  // Holds the current banner image number, it is related to  the class "banner-"
+  var currentImage = 1;
 
   // Changes the background and elements inside banner
   function changeBackground() {
-    switch (currentNumber) {
-      case 1:
-        banner.removeClass();
-        banner.addClass('service-1');
-        break;
-      case 2:
-        banner.removeClass();
-        banner.addClass('service-2');
-        break;
-      case 3:
-        banner.removeClass();
-        banner.addClass('service-3');
-        break;
-      case 4:
-        banner.removeClass();
-        banner.addClass('service-4');
-        break;
-    }
+    //Logs the the time of the image change
+    console.log('New image loaded');
+    //Clear all classes from the banner to remove the previous background image
+    banner.removeClass();
+    //Add a class with the new background image
+    banner.addClass('banner-' + currentImage);
   }
 
-  changeBackground();
+  //Loop function that switches images
+  function slideShow() {
+    // Call a 3s setTimeout when the loop is called
+    setTimeout(function() {
+      // Change background
+      changeBackground();
+      currentImage++;
+      //  if the index < the number of the services, call the loop function
+      if (currentImage < services.length + 1) {
+        slideShow(); //  ..  again which will trigger another
+      } else {
+        // Return to first image by setting the currentImage to 1
+        currentImage = 1;
+        slideShow();
+      }
+    }, 3000) //  ..  setTimeout() delay between image switching
+  }
+
+  // Call change background to set the first default image
+  changeBackground()
+  // Set the currentImage to next one to prevent double loading of the first image
+  currentImage++;
+  // Execute the slide show
+  slideShow();
 });
