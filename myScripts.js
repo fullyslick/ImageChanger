35dea598e6e2e8dd499ab02812cf3830ff736cb4
fullyslick <default@mail.com>
2018-03-01 17:21:49 +0200

    Add on click of 'image' behavior
    On click of any small image below the banner, the slide show stops, and
    the banner changes depending on the clicked image

--- myScripts.js
+++ myScripts.js
@@ -11,22 +11,18 @@ $(document).ready(function() {
   let previousBanner = 0;
   // Change this to set the delay time between switching banners
   const delayTime = 3000;
-
+  // Determines if the anchor is clicked
+  let isAnchorClicked = false;
 
   // Changes the background and elements inside banner
   function changeBackground() {
     // Remove the "active" class from all 'smallImages'
     // Remove the "show" class from all 'banners'
+    // Remove the "behind" class from all 'banners'
     for (let i = 0; i < smallImages.length; i++) {
       $(smallImages[i]).removeClass('active');
       $(banner[i]).removeClass('show');
-    }
-
-    // Get the position of the previous banner
-    previousBanner = currentBanner - 1;
-    if (previousBanner == -1) {
-      // If this is the first banner to be displayed, then previous is actually the last one
-      previousBanner = smallImages.length - 1;
+      $(banner[i]).removeClass('behind');
     }
 
     // Sets the "behind" class for the previous banner,
@@ -39,44 +35,74 @@ $(document).ready(function() {
     // Makes only current banner visible
     $(banner[currentBanner]).addClass('show');
 
-     // Since the current banner is visible,
-     // remove the previous banner
-     setTimeout(function functionName() {
-       $(banner[previousBanner]).removeClass('behind')
-     }, delayTime)
+    // If the anchor is not clicked then a slideShow is running
+    // Since the current banner is visible,
+    // remove the previous banner with a delay
+    if (!isAnchorClicked) {
+
+      setTimeout(function removeBehindClass() {
+        $(banner[previousBanner]).removeClass('behind')
+      }, delayTime)
+
+    }
   }
 
   // Switches images automaticly with a delay
   function slideShow() {
     // This will cause the delay between switching images
     setTimeout(function() {
-      // Increment the current image to switch background,
-      // On page load the first banner & service will be displayed because of the class - show & active
-      // because CSS is loaded before JS.
-      // So the slide show will begin with one position ahead
-      currentBanner++;
-      // Call changeBackground() to switch the images
-      changeBackground();
-      // Call the slideShow again, this creats loop effect with delay
-      if (currentBanner < smallImages.length) {
-        slideShow();
-      } else {
-        // It is now on the last image so reset the currentBanner to 0
-        // And start the slideShow again
-        currentBanner = 0;
-        changeBackground()
-        slideShow();
+
+      // If anchor is not clicked then run the slideShow
+      if (!isAnchorClicked) {
+
+        // Increment the current image to switch background,
+        // On page load the first banner & service will be displayed because of the class - show & active
+        // because CSS is loaded before JS.
+        // So the slide show will begin with one position ahead
+        currentBanner++;
+
+        // Get the position of the previous banner
+        previousBanner = currentBanner - 1;
+
+        if (previousBanner == -1) {
+
+          // If this is the first banner to be displayed, then previous is actually the last one
+          previousBanner = smallImages.length - 1;
+        }
+
+        // Call changeBackground() to switch the images
+        changeBackground();
+
+        // Call the slideShow again, this creats loop effect with delay
+        if (currentBanner < smallImages.length) {
+          slideShow();
+        } else {
+          // It is now on the last image so reset the currentBanner to 0
+          // And start the slideShow again
+          currentBanner = 0;
+          changeBackground()
+          slideShow();
+        }
+
       }
     }, delayTime) //  ..  The delay time between image switching
+
   }
 
   // Event listener for all anchors inside smallImages' lists
   for (let i = 0; i < smallImages.length; i++) {
 
-    $(smallImages[i]).on('click', 'a', function(e){
-      console.log(i);
-      });
-    }
+    $(smallImages[i]).on('click', 'a', function(e) {
+
+      isAnchorClicked = true;
+
+      previousBanner = currentBanner;
+
+      currentBanner = i;
+
+      changeBackground();
+    });
+  }
 
   // On page laod execite the slideShow, do not forget to set the first banner in html with class show & first li witch class "active"
   slideShow();
